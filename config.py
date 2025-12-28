89858eb5803b07ac4325d71b6342e009b6419c1a
LastPerson07 <sharmadhanpal950@gmail.com>
2025-12-28 09:18:34 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    APP_ID       = int(os.environ.get("APP_ID", ""))
+    API_HASH     = os.environ.get("API_HASH", "")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
