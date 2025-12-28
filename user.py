3361ca17a4c1d4b9781f055dae1794e4dc099244
LastPerson07 <sharmadhanpal950@gmail.com>
2025-12-28 09:37:37 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,34 +1,37 @@
-#----------------------------------- https://github.com/m4mallu/clonebot --------------------------------------------#
-import os
+# -----------------------------------
+# https://github.com/m4mallu/clonebot
+# -----------------------------------
 
+import os
 from pyrogram import Client
 
 if os.environ.get("ENV", False):
-    from sample_config import Config
-    from sample_config import LOGGER
+    from sample_config import Config, LOGGER
 else:
-    from config import Config
-    from config import LOGGER
+    from config import Config, LOGGER
+
 
 class User(Client):
     def __init__(self):
         super().__init__(
-            Config.TG_USER_SESSION,
-            api_hash=Config.API_HASH,
+            "user_session",  # 🔥 real filename
             api_id=Config.APP_ID,
+            api_hash=Config.API_HASH,
+            session_string=Config.TG_USER_SESSION,  # 🔥 session stored in memory
             workers=4
         )
         self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
-        usr_bot_me = await self.get_me()
+
+        me = await self.get_me()
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(
-            f"@{usr_bot_me.username}  started!"
-        )
-        return self, usr_bot_me.id
+
+        self.LOGGER(__name__).info(f"@{me.username} user account started!")
+
+        return self, me.id
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
+        self.LOGGER(__name__).info("User stopped. Bye.")
